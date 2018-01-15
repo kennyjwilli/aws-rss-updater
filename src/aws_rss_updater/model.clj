b4b00e8f90f5f3ffb17a6de2fd82eb798a655567
Kenny Williams <kennyjwilli@gmail.com>
2018-01-14 23:09:51 -0800

    Fix mark-post-as-sent! not working correctly.

--- src/aws_rss_updater/model.clj
+++ src/aws_rss_updater/model.clj
@@ -5,7 +5,7 @@
 
 (comment
   {::email "your.email@example.com"
-   ::feeds [{:feed/url        ""
+   ::feeds [{:feed/url  ""
              :feed/sent #{""}}]}
   )
 
@@ -33,15 +33,28 @@
   (let [s3-client (s3/client (:region settings))]
     (s3/get-object s3-client (:bucket settings) state-file)))
 
-(defn mark-post-as-sent
+(defn index-data-by
+  "Like `group-by` except each value in the map is a single value, not a
+  collection."
+  [f coll]
+  (reduce (fn [indexed v]
+            (assoc indexed (f v) v)) {} coll))
+
+(defn mark-post-as-sent!
   [settings feed-url post-url]
   (let [s3-client (s3/client (:region settings))]
     (s3/update-object s3-client (:bucket settings) state-file
                       (fn [state]
-                        (update state ::feeds
-                                (fn [feeds]
-                                  (mapv (fn [{:keys [feed/url] :as feed}]
-                                          (if (= url feed-url)
-                                            (update feed :feed/sent conj post-url)
-                                            feed))
-                                        feeds)))))))
+                        (update
+                          state ::feeds
+                          (fn [feeds]
+                            (-> (index-data-by :feed/url feeds)
+                                (update feed-url
+                                        (fn [feed]
+                                          (-> feed
+                                              (update :feed/sent
+                                                      (fnil conj #{})
+                                                      post-url)
+                                              (assoc :feed/url feed-url))))
+                                (vals)
+                                (vec))))))))
